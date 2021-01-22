e537c566044e1bf129a35d8a69aae690c5d41548
DarkCat11 <66437800+DarkCat11@users.noreply.github.com>
2021-01-22 16:44:33 -0600

    Add files via upload

---
+++ Calculadora/Calculadora/Calculadora/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Operaciones
+    {
+        public double Suma(double a, double b)
+        {
+            return a + b;
+        }
+        public double Resta(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiplicación(double a, double b)
+        {
+            return a * b;
+        }
+        public double División(double a, double b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
